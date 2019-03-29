7e938ecbe9eba1644cc7878e3640e9713704c065
Naim-Bijapure <Naimbijapure7407@gmail.com>
2019-03-29 13:52:25 +0530

    Updates

---
+++ precache-manifest.38dbbb73c0ada85fa54b64ab19288779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb11acf4e0dde5799feb",
+    "url": "/cool-name/static/css/main.edfa46dc.chunk.css"
+  },
+  {
+    "revision": "cb11acf4e0dde5799feb",
+    "url": "/cool-name/static/js/main.cb11acf4.chunk.js"
+  },
+  {
+    "revision": "9a83c47ff0c4f16f4b68",
+    "url": "/cool-name/static/js/1.9a83c47f.chunk.js"
+  },
+  {
+    "revision": "7a126ff3f996185c4c96",
+    "url": "/cool-name/static/js/runtime~main.7a126ff3.js"
+  },
+  {
+    "revision": "232bc8a6de3578fb5f0b16aec0b1cfc0",
+    "url": "/cool-name/index.html"
+  }
+];
